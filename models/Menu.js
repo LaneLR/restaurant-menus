3557ff9b4e8e3b8958988fd102a5e3c7ff10e588
LaneLR <laneleerichardson@gmail.com>
2025-05-13 16:36:53 -0500

    setup db.js and Menu & Restaurant models

--- models/Menu.js
+++ models/Menu.js
@@ -1,6 +1,13 @@
 const {sequelize} = require('../db');
 const { Sequelize } = require('sequelize');
 
-// TODO - create a Menu model
+class Menu extends Model {};
+
+Menu.init({
+    title: DataTypes.STRING,
+}, {
+    sequelize: sequelize,
+    modelName: "Restuarant",
+})
 
 module.exports = {Menu};
--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,6 +1,15 @@
 const {sequelize} = require('../db');
-const { Sequelize } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 
-// TODO - create a Restaurant model
+class Restaurant extends Model {};
+
+Restaurant.init({
+    name: DataTypes.STRING,
+    location: DataTypes.STRING,
+    cuisine: DataTypes.STRING,
+}, {
+    sequelize: sequelize,
+    modelName: "Restuarant",
+})
 
 module.exports = {Restaurant};
